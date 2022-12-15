3092908493388ccba9f8e5b7cdbf5d5c0a07d6c8
Karoline Amaral <karolinearaujo852@gmail.com>
2022-12-15 19:46:23 -0300

    commit final

--- src/app.js
+++ src/app.js
@@ -2,11 +2,9 @@ require('dotenv-safe').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
-//const swaggerUi = require('swagger-ui-express')
-//const swaggerFile = require('../swagger/swagger_output.json')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger/swagger_output.json')
 const index = require('./routes/index')
-//const projetosRoutes = require('./routes/projetosRoutes')
-//const usuariosRoutes = require('./routes/usuariosRoutes')
 const app = express()
 
 mongoose.connect()
@@ -14,10 +12,10 @@ mongoose.connect()
 app.use(cors())
 app.use(express.json())
 
-//app.use('/', usuariosRoutes)
+
 app.use('/', index)
-//app.use('/', projetosRoutes)
-//app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 
 module.exports = app
--- src/controller/instituicoesController.js
+++ src/controller/instituicoesController.js
@@ -1,93 +1,85 @@
-const InstituicoesModel = require('../models/instituicoesModels')
-const SECRET = process.env.SECRET
-const jwt = require('jsonwebtoken')
-
-
-
-
+const InstituicoesModel = require("../models/instituicoesModels");
+const SECRET = process.env.SECRET;
+//const jwt = require("jsonwebtoken");
 
 const findAllInstituicoes = async (req, res) => {
-    const instituicoes = await InstituicoesModel.find()
-    res.status(200).json(instituicoes)
-
-}
+  const instituicoes = await InstituicoesModel.find();
+  res.status(200).json(instituicoes);
+};
 
 const registerNewInstituicao = async (req, res) => {
+  try {
+ /*   const authHeader = req.get("authorization");
 
-
-    try {
-
-        const authHeader = req.get('authorization')
-
-        if (!authHeader) {
-            return res.status(401).send('You need an authorization')
-        }
-
-        const token = authHeader.split(' ')[1]
-         jwt.verify(token, SECRET, async function (error) {
-
-            if (error) {
-                return res.status(403).send('Access denied')
-            }
-        })
-
-        const { nome, contato, endereco, regiao, atuacao } = req.body
-        
-       const newInstituicao = new InstituicoesModel ({
-            nome, contato, endereco, regiao, atuacao
-        })
-    
-       const savedInstituicao = await newInstituicao.save()
-    
-       res.status(201).json(savedInstituicao)
-    
-       
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: error.message })
+    if (!authHeader) {
+      return res.status(401).send("You need an authorization");
     }
-}
+
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRET, async function (error) {
+      if (error) {
+        return res.status(403).send("Access denied");
+      }
+    });
+*/
+    const { nome, contato, endereco, regiao, atuacao } = req.body;
+
+    const newInstituicao = new InstituicoesModel({
+      nome,
+      contato,
+      endereco,
+      regiao,
+      atuacao,
+    });
+
+    const savedInstituicao = await newInstituicao.save();
+
+    res.status(201).json(savedInstituicao);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 const deleteInstituicao = async (req, res) => {
-    const { id } = req.params
-    await InstituicoesModel.findByIdAndDelete(id)
-    const message = `Instituição com o id: ${id} removido com sucesso.`
-    res.status(200).json({ message })
-
-}
+  const { id } = req.params;
+  await InstituicoesModel.findByIdAndDelete(id);
+  const message = `Instituição com o id: ${id} removido com sucesso.`;
+  res.status(200).json({ message });
+};
 const updateinstituicoes = async (req, res) => {
+  try {
+    const authHeader = req.get("authorization");
 
+    if (!authHeader) {
+      return res.status(401).send("You need an authorization");
+    }
 
-    try {
+    const token = authHeader.split(" ")[1];
+    await jwt.verify(token, SECRET, async function (error) {
+      if (error) {
+        return res.status(403).send("Access denied");
+      }
 
-        const authHeader = req.get('authorization')
-
-        if (!authHeader) {
-            return res.status(401).send('You need an authorization')
+      const { nome, contato, endereco, regiao, atuacao } = req.body;
+      const updateinstituicoes = await InstituicoesModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          nome,
+          contato,
+          local,
+          regiao,
         }
-
-        const token = authHeader.split(' ')[1] 
-             await jwt.verify(token, SECRET, async function (error) {
-
-            if (error) {
-                return res.status(403).send('Access denied')
-            }
-
-            const { nome, contato, endereco, regiao, atuacao} = req.body
-            const updateinstituicoes = await InstituicoesModel.findByIdAndUpdate(req.params.id, {
-             nome, contato, local, regiao,
-            })
-           res.status(200).json(updateinstituicoes)
-        })
-
-    } catch (error) {
-        console.error(error)
-        res.status(500).json([message,error.message])
-}
-
+      );
+      res.status(200).json(updateinstituicoes);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json([message, error.message]);
+  }
+};
 module.exports = {
-    findAllInstituicoes,
-    registerNewInstituicao,
-    deleteInstituicao,
-    updateinstituicoes,
-    
-}}
+  findAllInstituicoes,
+  registerNewInstituicao,
+  deleteInstituicao,
+  updateinstituicoes,
+};
